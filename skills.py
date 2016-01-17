dba90fad6976940a6edb600f18adbe9fcec16b29
Shalini Pyapali <shalinipyapali@Shalinis-MacBook-Pro.local>
2016-01-17 14:11:21 -0800

    completed some functions using dictionaries

---
+++ skills.py
@@ -0,0 +1,267 @@
+"""Skills-dictionaries.
+
+  IMPORTANT: these problems are meant to be solved using dictionaries and sets.
+"""
+
+
+def without_duplicates(words):
+    """Given a list of words, return the list with duplicates removed.
+
+    For example:
+
+        >>> sorted(without_duplicates(
+        ...     ["rose", "is", "a", "rose", "is", "a", "rose"]))
+        ['a', 'is', 'rose']
+
+    You should treat differently-capitalized words as different:
+
+        >>> sorted(without_duplicates(
+        ...     ["Rose", "is", "a", "rose", "is", "a", "rose"]))
+        ['Rose', 'a', 'is', 'rose']
+
+    """
+    words = set(words)
+    words = list(words)
+
+    return words
+
+
+def find_unique_common_items(list1, list2):
+    """Produce the set of *unique* common items in two lists.
+
+    Given two lists, return a list of the *unique* common items shared between
+    the lists.
+
+    IMPORTANT: you may not use 'if ___ in ___' or the method 'index'.
+
+    This should find [1, 2]:
+
+        >>> sorted(find_unique_common_items([1, 2, 3, 4], [2, 1]))
+        [1, 2]
+
+    However, now we only want unique items, so for these lists, don't show
+    more than 1 or 2 once:
+
+        >>> sorted(find_unique_common_items([4, 3, 2, 1], [1, 1, 2, 2]))
+        [1, 2]
+
+    """
+
+    #I'm going to use a dictionary to solve this. 
+
+    list1 = set(list1) - ((set(list1) - set(list2)))
+    list1 = list(list1)
+    return list1 
+
+
+
+
+def count_unique(input_string):
+    """Count unique words in a string.
+
+    This function should take a single string and return a dictionary
+    that has all of the distinct words as keys, and the number of times
+    that word appears in the string as values.
+
+
+    For example:
+
+        >>> print_dict(count_unique("each word appears once"))
+        {'appears': 1, 'each': 1, 'once': 1, 'word': 1}
+
+    Words that appear more than once should be counted each time:
+
+        >>> print_dict(count_unique("rose is a rose is a rose"))
+        {'a': 2, 'is': 2, 'rose': 3}
+
+    It's fine to consider punctuation part of a word (e.g., a comma
+    at the end of a word can be counted as part of that word) and
+    to consider differently-capitalized words as different:
+
+        >>> print_dict(count_unique("Porcupine see, porcupine do."))
+        {'Porcupine': 1, 'do.': 1, 'porcupine': 1, 'see,': 1}
+
+    """
+    input_list = input_string.split(" ")
+    unique_dictionary = {}
+    for word in input_list:
+        if word not in unique_dictionary:
+            unique_dictionary[word] = 1
+        else:
+            unique_dictionary[word] += 1  
+
+
+    return unique_dictionary
+
+
+def translate_to_pirate_talk(phrase):
+    """Translate phrase to pirate talk.
+
+    Given a phrase, translate each word to the Pirate-speak equivalent.
+    Words that cannot be translated into Pirate-speak should pass through
+    unchanged. Return the resulting sentence.
+
+    Here's a table of English to Pirate translations:
+
+    English     Pirate
+    ----------  ----------------
+    sir         matey
+    hotel       fleabag inn
+    student     swabbie
+    boy         matey
+    madam       proud beauty
+    professor   foul blaggart
+    restaurant  galley
+    your        yer
+    excuse      arr
+    students    swabbies
+    are         be
+    lawyer      foul blaggart
+    the         th'
+    restroom    head
+    my          me
+    hello       avast
+    is          be
+    man         matey
+
+    For example:
+
+        >>> translate_to_pirate_talk("my student is not a man")
+        'me swabbie be not a matey'
+
+    You should treat words with punctuation as if they were different
+    words:
+
+        >>> translate_to_pirate_talk("my student is not a man!")
+        'me swabbie be not a man!'
+
+    """
+    english_to_pirate_dic = {"sir": "matey", "hotel": "fleabag inn", "student" : "swabbie", "boy" : "matey", "madam" : "proud beauty",
+                            "professor" : "foul blaggart", "restaurant": "galley", "your" : "yer", "excuse" : "arr", "students" : "swabbies",
+                            "are": "be", "lawyer" : "foul blaggart", "the" : "th'", "restroom": "head", "my" : "me", "hello" : "avast", 
+                            "is":"be", "man" : "matey"}
+    input_list = phrase.split(" ")
+    pirate_list = []
+    for word in input_list:
+        if word in english_to_pirate_dic:
+            pirate_list.append(english_to_pirate_dic[word])
+        else:
+            pirate_list.append(word)
+
+    pirate_string = " ".join(pirate_list)
+
+
+
+    return pirate_string 
+
+
+def sort_by_word_length(words):
+    """Given list of words, return list of ascending [(len, [words])].
+
+    Given a list of words, return a list of tuples, ordered by word-length.
+    Each tuple should have two items---the length of the words for that
+    word-length, and the list of words of that word length.
+
+    For example:
+
+        >>> sort_by_word_length(["ok", "an", "apple", "a", "day"])
+        [(1, ['a']), (2, ['ok', 'an']), (3, ['day']), (5, ['apple'])]
+
+    """
+
+    word_length_dic = {}
+    ascending_length_list = []
+    #dictionary contains a key, value pair. 
+    for word in words:
+        word_length = len(word)
+        if word_length not in word_length_dic:
+            word_length_dic[len(word)] = [word]
+        else:
+            word_length_dic[len(word)].append(word)
+    
+    for word_tuple in word_length_dic.items():
+        ascending_length_list.append(word_tuple)
+
+
+
+
+    return ascending_length_list
+
+
+def get_sum_zero_pairs(input_list):
+    """Given list of numbers, return list of pair summing to 0.
+
+    Given a list of numbers, add up each individual pair of numbers.
+    Return a list of each pair of numbers that adds up to 0.
+
+
+    For example:
+
+        >>> sort_pairs( get_sum_zero_pairs([1, 2, 3, -2, -1]) )
+        [[-2, 2], [-1, 1]]
+
+        >>> sort_pairs( get_sum_zero_pairs([3, -3, 2, 1, -2, -1]) )
+        [[-3, 3], [-2, 2], [-1, 1]]
+
+    This should always be a unique list, even if there are
+    duplicates in the input list:
+
+        >>> sort_pairs( get_sum_zero_pairs([1, 2, 3, -2, -1, 1, 1]) )
+        [[-2, 2], [-1, 1]]
+
+    Of course, if there are one or more zeros to pair together,
+    that's fine, too (even a single zero can pair with itself):
+
+        >>> sort_pairs( get_sum_zero_pairs([1, 2, 3, -2, -1, 1, 1, 0]) )
+        [[-2, 2], [-1, 1], [0, 0]]
+
+    """
+    set_zero = set()
+    unique_zero_dic = {}
+    list_set_zero = []
+
+
+    for num in input_list:
+        if -num in input_list:
+            unique_zero_dic[num] = -num 
+        elif num == 0:
+            unique_zero_dic[num] == 0
+        else:
+            pass 
+
+    for element in unique_zero_dic.items():
+        element = sorted(element)
+        element = tuple(element)
+        set_zero.add(element)
+
+    for set_tuple in set_zero:
+        set_list = list(set_tuple)
+        list_set_zero.append(set_list)
+
+    return list_set_zero
+
+##############################################################################
+# You can ignore everything below this.
+
+def print_dict(d):
+    # This method is just used to print dictionaries in key-alphabetical
+    # order, and is only used for our documentation tests. You can ignore it.
+    if isinstance(d, dict):
+        print "{" + ", ".join("%r: %r" % (k, d[k]) for k in sorted(d)) + "}"
+    else:
+        print d
+
+
+
+def sort_pairs(l):
+    # Print sorted list of pairs where the pairs are sorted. This is used only
+    # for documentation tests. You can ignore it.
+    return sorted(sorted(pair) for pair in l)
+
+
+if __name__ == "__main__":
+    print
+    import doctest
+    if doctest.testmod().failed == 0:
+        print "*** ALL TESTS PASSED ***"
+    print
